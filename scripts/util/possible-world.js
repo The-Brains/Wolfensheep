0f7b232fc0134decce66df53ac895e6a967627ab
Leo Benkel <leo@thumbtack.com>
2017-05-09 18:17:14 -0700

    rename files and add worlds parameters

---
+++ scripts/util/possible-world.js
@@ -0,0 +1,31 @@
+define({
+    'humidity': {
+        'dry',
+        'normal',
+        'humid',
+    },
+    'temperature': {
+        'hot',
+        'warm',
+        'normal',
+        'cold',
+        'freezing',
+    },
+    'ground': {
+        'sand',
+        'dirt',
+        'mud',
+        'water',
+        'rock',
+    },
+    'wind': {
+        'quiet',
+        'windy',
+        'stormy',
+    },
+    'cloud': {
+        'overcast',
+        'clear',
+        'rainy',
+    }
+});
