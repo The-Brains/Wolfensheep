42b958b4c961d30af679e8be06e26d86fe4520f9
Jack Le Hamster <jacklehamster@gmail.com>
2017-06-03 22:48:22 -0700

    Enforce define

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 // except for 'app' ones, which are in a sibling
 // directory.
 requirejs.config({
-//    enforceDefine: true,
+    enforceDefine: true,
     baseUrl: 'scripts/lib',
     paths: {
         jquery: [
