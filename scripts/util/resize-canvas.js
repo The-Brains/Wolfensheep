36ee5059973ad30725cafcb2f48416ae7a39f4b6
Leo Benkel <leo.benkel@gmail.com>
2017-06-05 12:54:23 -0700

    Fix resize canvas

--- scripts/util/resize-canvas.js
+++ scripts/util/resize-canvas.js
@@ -1,18 +1,20 @@
 define(['jquery', 'lodash'], function($, _) {
     function resizeCanvas() {
         var canvasDrawing = $('#canvas');
-        var width = canvasDrawing.width();
-        var ctxDrawing = canvasDrawing[0].getContext('2d');
+        var container = $('.page-content');
 
-        var height = $(window).height() - $('header').height();
-        CANVAS_WIDTH = width;
-        CANVAS_HEIGHT = height;
+        var height = container.height();
+        var width = container.width();
+
         $('.CanvasArea').height(height);
-        ctxDrawing.canvas.width = width;
-        ctxDrawing.canvas.height = height;
+        $('.CanvasArea').width(width);
+        canvasDrawing.width(width);
+        canvasDrawing.height(height);
+        console.log('resized: W:' + width + ', H: ' + height);
     };
 
     $(window).resize(_.debounce(resizeCanvas, 200, {
         maxWait: 1000,
     }));
+    resizeCanvas();
 });
