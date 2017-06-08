3e1c0eac85e1713cab1044d9408a308d758350d0
Jack Le Hamster <jacklehamster@gmail.com>
2017-06-07 22:37:21 -0700

    Tilesview, agents view performance fix

--- scripts/graphics/agentsview.js
+++ scripts/graphics/agentsview.js
@@ -2,44 +2,54 @@ define([
         'threejs',
         'dobuki',
         './imagestore.js',
+        '../core/localization/location.js',
     ],
-    function(THREE, DOK, ImageStore) {
+    function(THREE, DOK, ImageStore, Location) {
         var AgentsView = function (cameraHandler, spriteRenderer, cellSize, game) {
+            var range = 100;
             var worldWidth = game.getWidth();
             var worldHeight = game.getHeight();
 
-            var collection = new DOK.Collection(
-                {
-                    agents: null,
-                },
-                function (agent) {
-                    var img = ImageStore.getImageFromAgent(agent);
-                    var x = agent.getLocation().getX();
-                    var y = agent.getLocation().getY();
+            var tempArray = [];
 
-                    var spriteObj = DOK.SpriteObject.create().init(
-                        x * cellSize, y * cellSize, cellSize,
-                        cellSize*3, cellSize*3,
-                        null,
-                        1,
-                        img
-                    );
-                    spriteObj.type = 'face';
-                    return spriteObj;
+            var collection = new DOK.Collection({
+                    type: "grid",
+                    get x() {
+                        return cameraHandler.getCamPos().x - Math.floor(range / 2);
+                    },
+                    get y() {
+                        return cameraHandler.getCamPos().y - Math.floor(range / 2);
+                    },
+                    width: range,
+                    height: range,
+                    agentsCache: {},
                 },
-                function (callback) {
-                    var agents = this.options.agents;
-                    if (agents) {
-                        for (var i=0; i<agents.length; i++) {
-                            var sprite = this.getSprite(agents[i]);
-                            callback(sprite);
-                        }
+                function (x, y) {
+                    if(this.options.agentsCache[x+"-"+y] === undefined) {
+                        this.options.agentsCache[x+"-"+y] = game.getWorld().getAgentsAt(new Location(x,y));
                     }
+                    var agents = this.options.agentsCache[x+"-"+y];
+                    tempArray.length = 0;
+                    for(var a in agents) {
+                        var agent = agents[a];
+                        var img = ImageStore.getImageFromAgent(agent);
+
+                        var spriteObj = DOK.SpriteObject.create(
+                            x * cellSize, y * cellSize, cellSize,
+                            cellSize*3, cellSize*3,
+                            null,
+                            1,
+                            img
+                        );
+                        spriteObj.type = 'face';
+                        tempArray.push(spriteObj);
+                    }
+
+                    return tempArray;
                 }
             );
 
             function update() {
-                collection.options.agents = game.getWorld().getAllAgents();
                 collection.forEach(spriteRenderer.display);
             }
 
