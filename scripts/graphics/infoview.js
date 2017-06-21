6d24fe2906296ed83363062ebf6093c278cab8f7
Jack Le Hamster <jacklehamster@gmail.com>
2017-06-21 00:09:31 -0700

    fix infoview

--- scripts/graphics/infoview.js
+++ scripts/graphics/infoview.js
@@ -6,6 +6,8 @@ define([
     function(DOK, ImageStore, Location) {
         return function (cameraHandler, game) {
             var roundabout = new DOK.Utils.Roundabout();
+            var worldWidth = game.getWidth();
+            var worldHeight = game.getHeight();
 
             function getClosestAgentsPosition(x,y,limit) {
                 x = Math.round(x);
@@ -14,7 +16,9 @@ define([
 
                 for(var i=0; i<limit; i++) {
                     var pos = roundabout.next();
-                    var loc = new Location(x+pos[0],y+pos[1]);
+                    var tileX = (((x+pos[0]) % worldWidth) + worldWidth) % worldWidth;
+                    var tileY = (((y+pos[1]) % worldHeight) + worldHeight) % worldHeight;
+                    var loc = new Location(tileX,tileY);
                     var agents = game.getWorld().getAgentsAt(loc);
                     if(agents) {
                         for(var a in agents) {
